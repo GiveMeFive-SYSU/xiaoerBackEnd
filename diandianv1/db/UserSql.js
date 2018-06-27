97c616ca51d6792cb7c5c399bec6071352005303
jiaxin96 <572673807@qq.com>
2018-06-27 21:56:43 +0800

    更改店铺名成功

--- diandianv1/db/UserSql.js
+++ diandianv1/db/UserSql.js
@@ -3,7 +3,7 @@ var UserSQL = {
     // queryAll:'SELECT Username,Shopname,Tablenum,Phonenum,Key FROM business_info',
     getUserByUsername:'SELECT * FROM business_info WHERE Username = ? ',
     getShopNameByUsername:'SELECT Shopname FROM business_info WHERE Username = ? ',
-    updateShopName:'UPDATE business_info SET Shopname = ? WHERE Username=?'
+    updateShopName:'UPDATE business_info SET Shopname = ? WHERE Username= ?'
 };
 // param.username, password, param.shopname, param.tablenum, param.phonenum, Key
 module.exports = UserSQL;
