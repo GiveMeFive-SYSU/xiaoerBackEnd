d130e15a9b7a32ea134ef9b83fdd20455b9c6f8b
YoshiPark <907473001@qq.com>
2018-06-27 20:38:19 +0800

    更改商铺名字

--- diandianv1/db/UserSql.js
+++ diandianv1/db/UserSql.js
@@ -2,7 +2,8 @@ var UserSQL = {
     insert:'INSERT INTO business_info(Username,Password,Shopname,Phonenum,accessKey) VALUES(?,?,?,?,?)',
     // queryAll:'SELECT Username,Shopname,Tablenum,Phonenum,Key FROM business_info',
     getUserByUsername:'SELECT * FROM business_info WHERE Username = ? ',
-    getShopNameByUsername:'SELECT Shopname FROM business_info WHERE Username = ? '
+    getShopNameByUsername:'SELECT Shopname FROM business_info WHERE Username = ? ',
+    updateShopName:'UPDATE business_info SET Shopname = ? WHERE Username=?'
 };
 // param.username, password, param.shopname, param.tablenum, param.phonenum, Key
 module.exports = UserSQL;
