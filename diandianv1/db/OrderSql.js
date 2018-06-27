4edd3df133e5abdca7e33bdcc15e11b4efc5e7f7
YoshiPark <907473001@qq.com>
2018-06-27 11:56:58 +0800

    修改mysql订单根据桌号升序，订单时间降序进行排序返回

--- diandianv1/db/OrderSql.js
+++ diandianv1/db/OrderSql.js
@@ -4,7 +4,7 @@
 
 var OrderSql = {
     insert:'INSERT INTO business_order(Username, Ordernumber, Ordertime, Tablenumber, Tastenote,Price) VALUES(?,?,?,?,?,?)',
-    getOrderByUsername:'SELECT * FROM business_order WHERE Username = ? ORDER BY Ordertime desc',
+    getOrderByUsername:'SELECT * FROM business_order WHERE Username = ? ORDER BY Tablenumber asc, Ordertime desc',
     deleteOrderInfo:'DELETE FROM business_order WHERE Username=? AND Ordernumber=?'
 };
 module.exports = OrderSql;
