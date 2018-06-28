55ca830fad4663e36efad297f10e4154512345b7
jiaxin96 <572673807@qq.com>
2018-06-28 15:37:28 +0800

    syc 6.28 -1

--- diandianv1/server/serverConfig.js
+++ diandianv1/server/serverConfig.js
@@ -1,7 +1,7 @@
 module.exports =
     {
         diandianserver: {
-            host: 'http://172.19.53.217',
+            host: 'http://172.19.53.82',
             port: 3000,
             imgDis:'images'
         }
